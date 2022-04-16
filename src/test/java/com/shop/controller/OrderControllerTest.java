0cb324475658abad5842883daacd2c7d3c3692dd
leewoonhyoung <wh3619@gmail.com>
2022-04-17 00:43:02 +0900

    2022-04-17 상품 등록 및 조회 추가 + querydsl + test code

---
+++ src/test/java/com/shop/controller/OrderControllerTest.java
@@ -0,0 +1,32 @@
+package com.shop.controller;
+
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderRepository;
+import com.shop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class OrderControllerTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+}
