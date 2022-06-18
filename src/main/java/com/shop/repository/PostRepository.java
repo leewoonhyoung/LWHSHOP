d7e6815cd553f16790a5c7c7cafc92339e26d5a2
leewoonhyoung <wh3619@gmail.com>
2022-06-18 15:36:36 +0900

    2022-06-18 redit post 기능 추가

--- src/main/java/com/shop/repository/PostRepository.java
+++ src/main/java/com/shop/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package com.shop.repository;
 
+import com.shop.entity.Member;
+import com.shop.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllBySubreddit(Subreddit subreddit);
 
-    List<Post> findByUser(User user);
+    List<Post> findByUser(Member member);
 }
 
