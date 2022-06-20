98c98a063dcefc44842ed412d4f16aee2c440691
leewoonhyoung <wh3619@gmail.com>
2022-06-20 20:45:57 +0900

    2022-06-20 ERD 재구성해보기

---
+++ src/main/java/com/shop/entity/Subreddit.java
@@ -0,0 +1,43 @@
+package com.shop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Community name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+
+    //@joincolumn 추가하기
+    @OneToMany(fetch = LAZY)
+    private List<Post> posts;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+}
+
--- src/main/java/com/shop/repository/PostRepository.java
+++ src/main/java/com/shop/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.shop.repository;
 
 import com.shop.entity.Member;
 import com.shop.entity.Post;
+import com.shop.entity.Subreddit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
